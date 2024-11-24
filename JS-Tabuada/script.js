7db755650f9329d2e7649e7bfe8c27c19ca1b0e1
Cauã Fortes <143011839+cauafortes@users.noreply.github.com>
2024-11-24 13:58:53 -0300

    Add files via upload

---
+++ JS-Tabuada/script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    let numero = Number(document.getElementById('numero').value);
+    let resultado = document.getElementById('resultado');
+
+    if (numero > 0) {
+        let tabuada = '';
+        for (let i = 1; i <= 10; i++) {
+            tabuada += `${i} x ${numero} = ${i * numero}<br>`;
+        }
+        resultado.innerHTML = tabuada;
+    } else {
+        resultado.innerHTML = 'Insira um número positivo.';
+    }
+}
